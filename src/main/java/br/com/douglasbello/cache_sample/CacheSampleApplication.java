ef40797c50ea511ec35d9ed91a146bbb00c2a0f2
Douglas Bello <dgbello.dev@hotmail.com>
2024-07-17 09:13:23 -0300

    ini

---
+++ src/main/java/br/com/douglasbello/cache_sample/CacheSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.douglasbello.cache_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheSampleApplication.class, args);
+	}
+
+}
